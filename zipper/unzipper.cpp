91935654244ab5a853fd27192733dc82e0ef5953
LeCrapouille <lecrapouille@gmail.com>
2019-04-11 23:07:14 +0200

    Fix folder separators #32

--- zipper/unzipper.cpp
+++ zipper/unzipper.cpp
@@ -1,4 +1,5 @@
 #include "unzipper.h"
+#include "CDirEntry.h"
 #include "defs.h"
 #include "tools.h"
 
@@ -416,7 +417,7 @@ namespace zipper {
         if (!locateEntry(it->name))
           continue;
 
-        std::string alternativeName = destination.empty() ? "" : destination + "/";
+        std::string alternativeName = destination.empty() ? "" : destination + CDirEntry::Separator;
 
         if (alternativeNames.find(it->name) != alternativeNames.end())
           alternativeName += alternativeNames.at(it->name);
@@ -431,7 +432,7 @@ namespace zipper {
 
     bool extractEntry(const std::string& name, const std::string& destination)
     {
-      std::string outputFile = destination.empty() ? name : destination + "\\" + name;
+      std::string outputFile = destination.empty() ? name : destination + CDirEntry::Separator + name;
 
       if (locateEntry(name))
       {
