37528d736020db6facf3ea21abc53b8c170ce5e9
Giuseppe Cesarano <pecorainpannacotta@gmail.com>
2020-09-17 13:02:13 +0200

    Appveyor fix

--- .integration/appveyor-launch_tests.sh
+++ .integration/appveyor-launch_tests.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-export PATH="/mingw64/bin:/usr/local/bin:/usr/bin:/bin:/c/WINDOWS/system32:/c/WINDOWS:/c/WINDOWS/System32/Wbem:/c/WINDOWS/System32/WindowsPowerShell/v1.0"
-bf=$(cygpath ${APPVEYOR_BUILD_FOLDER})
-cd "$bf" || (echo "Cannot go to directory $bf"; return 1)
+export PATH="/mingw64/bin"
+cd ${APPVEYOR_BUILD_FOLDER}
 
 # Compile
-mkdir -p build
+cmake -B build -GNinja .
 cd build || exit 1
-cmake .. || exit 1
-make -j4 || exit 1
+cmake --build . || exit 1
 cd ..
 
 # Unit test
